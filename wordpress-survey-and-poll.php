581389879c20ac6fdf0293bb8cff249616b9e9fc
pantherius <pantherius@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-02-09 08:51:37 +0000

    update
    git-svn-id: https://plugins.svn.wordpress.org/wp-survey-and-poll/trunk@1085379 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- wordpress-survey-and-poll.php
+++ wordpress-survey-and-poll.php
@@ -5,7 +5,7 @@ defined( 'ABSPATH' ) OR exit;
  * Plugin URI: http://sympies.com/modal-survey
  * Description: Add simple surveys to your website
  * Author: Pantherius
- * Version: 1.1.3
+ * Version: 1.1.4
  * Author URI: http://sympies.com
  */
  
