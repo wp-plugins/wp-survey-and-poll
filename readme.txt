8a2ba84d32698240fb8adc1e4ff936bcd97ae441
pantherius <pantherius@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-04-24 11:48:44 +0000

    improved styles and texts on admin
    git-svn-id: https://plugins.svn.wordpress.org/wp-survey-and-poll/trunk@1144588 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- readme.txt
+++ readme.txt
@@ -1,9 +1,9 @@
 === WordPress Survey & Poll ===
 Contributors: Pantherius
 Donate link: https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=6H6YHMQG8JJ5J&lc=USD&currency_code=USD&bn=PP%2dDonationsBF%3abtn_donateCC_LG%2egif%3aNonHosted
-Tags: poll, survey, feedback, user voice, user satisfaction, marketing, questions, answers, wordpress quiz
+Tags: poll, survey, questionnaire, quiz, opinion, customer satisfaction, feedback, survey form, poll builder, marketing, questions, answers, polling, quizzes, data collection
 Requires at least: 3.9.0
-Tested up to: 4.1.2
+Tested up to: 4.2
 Stable tag: trunk
 License: GPLv2 or later
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
@@ -100,6 +100,9 @@ You can export the survey to PDF, XML, JSON, CSV and XLS with the [PRO version](
 Initial Release
 
 == Changelog ==
+
+= 1.2.6 =
+* Increased questions and answers limits, improved style and texts on admin
 
 = 1.2.3 =
 * Improved Admin Area
--- templates/acustom.php
+++ templates/acustom.php
@@ -6,14 +6,15 @@ print('
 		<p>for downloading this plugin. WP Survey and Poll is a limited version, even you can reach many of custom features. If you really would like to get much more customization options, <br>purchase the <a target="_blank" href="http://pantherius.com/modal-survey">PRO version</a> for only a one-time fee: $17</p>
 		<h4>Pro Version Features</h4>
 		<ul>
-			<li>- Unlimited Questions and Answers</li>
+			<li>- <strong>Unlimited</strong> Questions and Answers, Use <strong>Multiple Instances</strong> on the same page</li>
 			<li>- Display the Results with <strong>Plain Text</strong>, <strong>Progress Bar</strong>, <strong>Line Bar</strong>, <strong>Pie Chart</strong>, <strong>Bar Chart</strong>, <strong>Doughnut Chart</strong>, <strong>Line Chart</strong>, <strong>Polar Chart</strong> and <strong>Radar Chart</strong></li>
 			<li>- Export the survey to <strong>PDF</strong>, <strong>XML</strong>, <strong>CSV</strong>, <strong>XLS</strong> and <strong>JSON</strong></li>
-			<li>- Offer link at the end of the Survey</li>
-			<li>- Hide the bottom line \'Powered By\' text</li>
-			<li>- Integrated Shortcode Manager</li>
-			<li>- Free Updates and Support</li>
+			<li>- <strong>Import Survey</strong>, <strong>Duplicate</strong> Survey, Offer link at the end</li>
+			<li>- Allows <strong>Multiple Choices</strong> (required and choosable number of answers)</li>
+			<li>- Integrated <strong>Shortcode Manager</strong>, add survey or results shortcodes to text <strong>widget</strong></li>
+			<li>- Hide the bottom line \'Powered By\' text, Premium <strong>Support and Updates</strong></li>
 		</ul>
+		<a class="close button button-default" href="#">Use Free Version</a>
 		<a class="close button button-default" href="#">Use Free Version</a>
 		<a class="close button button-primary" href="http://codecanyon.net/item/modal-survey-wordpress-feedbacks-polls-plugin/6533863?ref=pantherius">Purchase PRO Version - $17</a>
 	 </div>
